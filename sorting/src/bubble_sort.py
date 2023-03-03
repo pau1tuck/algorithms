40da8c6b8e086c5b212aa6794258a999407b0d50
Paul Tuck <paul.tuck@oxon.org>
2023-03-04 03:20:18 +0700

    "2023-03-04T03:20+07"

--- sorting/src/bubble_sort.py
+++ sorting/src/bubble_sort.py
@@ -1,10 +1,21 @@
+import time
+
+st = time.time()
+
+
 def bubble_sort(my_list):
-    for i in range(len(my_list) - 1, 0 ,-1):
+    for i in range(len(my_list) - 1, 0, -1):
         for j in range(i):
-            if my_list[j] > my_list[j+1]:
-                temp = my_list[j]
-                my_list[j] = my_list[j+1]
-                my_list[j+1] = temp
+            if my_list[j] > my_list[j + 1]:
+                tmp = my_list[j]
+                my_list[j] = my_list[j + 1]
+                my_list[j + 1] = tmp
     return my_list
 
-print(bubble_sort([4,2,6,5,1,3]))
+
+et = time.time()
+
+elapsed_time = et - st
+
+print(bubble_sort([4, 2, 6, 5, 1, 3, 8, 7]))
+print("Execution time:", elapsed_time, "seconds")
